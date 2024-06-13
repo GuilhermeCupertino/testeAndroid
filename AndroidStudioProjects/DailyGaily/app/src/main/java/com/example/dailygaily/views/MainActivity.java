6ffbe20f17f8ba56d9e397b26a35db5c05c9e833
GuilhermeCupertino <guilherme.cupertino@ufms.br>
2024-06-13 18:20:12 -0400

    primeiroCommit

---
+++ AndroidStudioProjects/DailyGaily/app/src/main/java/com/example/dailygaily/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dailygaily.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dailygaily.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
